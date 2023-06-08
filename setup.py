769404e982e7b1737764c24892799c3cf6c98b78
biadeneedo <biadene.edoardo@gmail.com>
2023-06-08 17:08:45 +0200

    folder relocation

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fin_hedge",
+    version="0.0.1",
+    author="edob",
+    author_email="biadene.edoardo@gmail.com",
+    description="A package for quant finance",
+    long_description="x",
+    long_description_content_type="text/markdown",
+    url="https://github.com/biadeneedo/fin_hedge",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
