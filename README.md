219a5830896fd2235b2c3fc147f1ac496e9a95b3
biadeneedo <biadene.edoardo@gmail.com>
2023-06-09 16:04:56 +0200

    restructuring  + readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# fin_hedge
+fin_hedge is a comprehensive suite of modules and tools designed to assist in the development, analysis, and execution of quantitative trading strategies for hedging applications in financial Markets.
+Some module is still in work in progress. The word # WIP indicates the modules that are not ready
+
+## Features
+The modules are divided in those features: 
+- **Data Gathering and Processing**: Easily collect and process financial data from various sources.  # WIP
+- **Pricing**: Utilize financial and computation models to define the correct price for an instrument and its expected value. 
+- **Portfolio Optimization**: Optimize portfolios using various strategies like mean-variance optimization and risk-parity. # WIP 
+- **Risk Analysis**: Utilize powerful risk models like Value-at-Risk (VaR) and Conditional Value-at-Risk (CVaR). # WIP
+- **Trading**: Backtest strategies, generate trade orders, and execute trades through supported brokerage APIs. # WIP
+- **Performance Evaluation**: Calculate important performance metrics like Sharpe ratio, alpha, beta, drawdown, etc. # WIP
+- **Visualization**: Generate insightful charts and plots for data analysis. # WIP
+- **Utilities**: Helpful utility functions and classes for common tasks such as statistical tests on variables.
+
+## Installation
+```bash
+pip install HedgeFundToolkit
+```
+
+## Usage
+Detailed usage for each module is outlined in the test section of the repo. # WIP
+
+## License
+This project is licensed under the terms of the MIT license. See [LICENSE](LICENSE) for details.
--- fin_hedge/pricing.py
+++ fin_hedge/pricing.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+
 
 """ Discounted Cash Flow (General)"""
 def dcf(cash_flows, discount_rate):
@@ -24,7 +28,7 @@ def dcf_bond(face_value, coupon_rate, payments_per_year, years_to_maturity, requ
     for i in range(1, int(years_to_maturity * payments_per_year) + 1):
         price += coupon_payment / (1 + required_yield / payments_per_year) ** i
     price += face_value / (1 + required_yield / payments_per_year) ** (years_to_maturity * payments_per_year)
-    print(f"The price of the bond is: ${round(bond_price, 2)}")
+    #print(f"The price of the bond is: ${round(bond_price, 2)}")
     return price
 
 """ Black-Scholes """
@@ -34,8 +38,53 @@ def euro_vanilla_call(S, K, T, r, q, sigma):
     call = S * np.exp(-q * T) * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0)
     return call
 
-
-
+""" LONG-SHORT TERM MEMORY NN"""
+def lstm(df):
+    # Convert data to numpy array
+    dataset = df.values
+    # Scale the data
+    scaler = MinMaxScaler(feature_range=(0,1))
+    scaled_data = scaler.fit_transform(dataset)
+    # Split data
+    training_data_len = int(np.ceil(len(dataset) * .8))
+    train_data = scaled_data[0:int(training_data_len), :]
+    x_train = []
+    y_train = []
+    # Train the data
+    for i in range(60, len(train_data)):
+        x_train.append(train_data[i-60:i, 0])
+        y_train.append(train_data[i, 0])
+    # Convert x_train and y_train to numpy arrays
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    # Reshape the data to 3D for LSTM model
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    # Build the LSTM model
+    model = Sequential()
+    model.add(LSTM(50, return_sequences=True, input_shape= (x_train.shape[1], 1)))
+    model.add(LSTM(50, return_sequences= False))
+    model.add(Dense(25))
+    model.add(Dense(1))
+    # Compile the model
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    # Train the model
+    model.fit(x_train, y_train, batch_size=1, epochs=1)
+    # Create testing dataset
+    test_data = scaled_data[training_data_len - 60: , :]
+    # Create datasets x_test and y_test
+    x_test = []
+    y_test = dataset[training_data_len:, :]
+    for i in range(60, len(test_data)):
+        x_test.append(test_data[i-60:i, 0])
+    # Convert x_test to a numpy array and reshape it to 3D
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    # Get predicted scaled price
+    predictions = model.predict(x_test)
+    predictions = scaler.inverse_transform(predictions)
+    # Get RMSE (root mean squared error)
+    rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
+    print('Root Mean Squared Error: ', rmse)
+    return prediction
 
 
 
@@ -69,3 +118,8 @@ if __name__ == '__main__':
     option_price = euro_vanilla_call(spot_price, strike_price, expiration_time, interest_rate, dividend_rate,
                                      volatility)
     print('The price of the call option is: $', round(option_price, 2))
+
+    # LSTM
+    df = yf.download('AAPL', start='2012-01-01', end='2023-06-07')
+    df = df.filter(['Close'])
+    prediction = lstm(df)
