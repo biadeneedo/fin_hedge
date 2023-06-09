8ffc4892755408a668ec134d7a51a52a86732b57
biadeneedo <biadene.edoardo@gmail.com>
2023-06-09 09:20:38 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fin_hedge",
-    version="0.0.1",
+    version="0.0.2",
     author="edob",
     author_email="biadene.edoardo@gmail.com",
     description="A package for quant finance",
